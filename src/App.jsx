d7872e88e1b0c8135fdd5cda3ef3c8c4d3e40e61
nmorcab2106 <amorac@iesrafaelalberti.es>
2026-05-07 13:33:15 +0200

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -8,23 +8,21 @@ import FavoritesPage from './pages/FavoritesPage';
 import { FavoritesProvider } from './context/FavoritesContext';
 
 function App() {
-  return (
-    <FavoritesProvider>
-      <Router>
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-          <Navbar />
-          <div style={{ flex: 1 }}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/:type/:name" element={<DetailPage />} />
-              <Route path="/favorites" element={<FavoritesPage />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    </FavoritesProvider>
-  );
+    return (
+        <FavoritesProvider>
+            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                <Navbar />
+                <div style={{ flex: 1 }}>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/:type/:name" element={<DetailPage />} />
+                        <Route path="/favorites" element={<FavoritesPage />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </FavoritesProvider>
+    );
 }
 
 export default App;
